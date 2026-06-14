81df440f612d9af24ec27ae46691f02fdf410b1f
Sauryaman Bisen <sauryamanbisen@gmail.com>
2026-06-14 12:47:32 +0530

    feat: define application action type constants

---
+++ js/context/actionTypes.js
@@ -0,0 +1,12 @@
+/**
+ * Action type constants to prevent reducer typo mistakes.
+ */
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const UPDATE_QUANTITY = "UPDATE_QUANTITY";
+export const CLEAR_CART = "CLEAR_CART";
+export const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
+export const SET_FILTERS = "SET_FILTERS";
+export const TOGGLE_VIEW = "TOGGLE_VIEW";
+export const APPLY_COUPON = "APPLY_COUPON";
+export const REMOVE_COUPON = "REMOVE_COUPON";
